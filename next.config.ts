1523bf91879894ee7ab7f950315cb77ad30b72cb
Chris Hall <chall@threesixtyproducts.com>
2025-07-18 13:44:50 -0400

    Remove webpack override to fix build

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,5 @@
 import type { NextConfig } from 'next';
-import type { Configuration } from 'webpack';
 
-const nextConfig: NextConfig = {
-  webpack: (config: Configuration) => {
-    if (!config.resolve) config.resolve = {};
-    if (!config.resolve.alias) config.resolve.alias = {};
-    config.resolve.alias['@'] = __dirname;
-    return config;
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
