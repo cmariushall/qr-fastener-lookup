4a5f682e19f4c1ab21eafd5795d72f19a008a943
Chris Hall <chall@threesixtyproducts.com>
2025-07-18 13:40:32 -0400

    Fix next.config.ts webpack typing

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config) => {
+import type { NextConfig } from 'next';
+import type { Configuration } from 'webpack';
+
+const nextConfig: NextConfig = {
+  webpack: (config: Configuration) => {
+    if (!config.resolve) config.resolve = {};
+    if (!config.resolve.alias) config.resolve.alias = {};
     config.resolve.alias['@'] = __dirname;
     return config;
   },
